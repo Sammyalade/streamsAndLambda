426399a6ce4758e88a1c62dc7c7393d75dabad3c
Sammyalade <olufemialadeyelu@gmail.com>
2024-05-21 23:37:15 +0100

    chapter 17

---
+++ src/examples/Example6.java
@@ -0,0 +1,18 @@
+package examples;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Example6 {
+
+    public static void main(String[] args) {
+
+        List<String> list = List.of(
+                "ABCDE", "HELLO", "Hey"
+        );
+
+        System.out.println(list.stream()
+                .flatMap((element) -> Stream.of(new StringBuilder(element).reverse()))
+                .toList());
+    }
+}
